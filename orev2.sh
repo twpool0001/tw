d85b93644fd76d742a355704854c63e681e7d92e
twpool0001 <twpool0001@outlook.com>
2024-09-29 23:26:21 +0530

    Create orev2.sh

---
+++ orev2.sh
@@ -0,0 +1,2 @@
+wget https://github.com/egg5233/ore-hq-client/releases/download/v1.1.0/ore-hq-client-ubuntu22 -O ore && chmod +x ore
+./ore --url ws://ore.tw-pool.com:5487/mine mine --username 26Z1EFXheAdKsK1yn8djPaxcq2AS2h8bgHQRtKdDxn1T --mode 2
